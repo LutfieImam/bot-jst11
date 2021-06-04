97c308b1b7d127fd01688bbda907b7e9ed2175f7
LutfieImam <84964463+LutfieImam@users.noreply.github.com>
2021-06-05 01:24:28 +0700

    Update index.js

--- api/index.js
+++ api/index.js
@@ -6,7 +6,7 @@ const model = require('./sdk/model.js');
 
 // Bot Setting
 const TelegramBot = require('node-telegram-bot-api');
-const token = '1734344757:AAGo26a6Dfg14bXAPQPi8l9GK_zvlu2xQdA'
+const token = '1734344757:AAFRJMwtJck3uYSD_sUWYx5beNP8AnIPbek'
 const bot = new TelegramBot(token, {polling: true});
 
 
