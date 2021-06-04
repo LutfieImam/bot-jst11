f5288381ad2f5c8e4402d8d959037aafccc9358e
LutfieImam <84964463+LutfieImam@users.noreply.github.com>
2021-06-05 01:56:55 +0700

    Update index.js

--- api/index.js
+++ api/index.js
@@ -54,21 +54,5 @@ bot.on('message', (msg) => {
     })
 }else{
        state = 0
-       bot.sendMessage(
-        msg.chat.id,
-        `kembali /start`
-    );
-
-// routers
-r.get('/prediction/:i/:r', function(req, res, next) {    
-    model.predict(
-        [
-            parseFloat(req.params.i), // string to float
-            parseFloat(req.params.r)
-        ]
-    ).then((jres)=>{
-        res.json(jres);
-    })
-});
-
-module.exports = r;
+       }
+})
